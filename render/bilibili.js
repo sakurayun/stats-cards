4de391e5528d11f0f00b7a2ac4b8ac1fc2be26d6
hiugo <30366696+sakurayun@users.noreply.github.com>
2022-09-11 17:43:17 +0800

    Update bilibili.js

--- render/bilibili.js
+++ render/bilibili.js
@@ -8,12 +8,12 @@ function renderBilibiliCard(data) {
     description += '...';
   }
   let items = [
-    constructItem(94, 44, `${name}&apos;s Bilibili Stats`, 'title', 18),
-    constructItem(55, 84, `Followers`, 'label', 13.5),
-    constructItem(203, 85, `Video Views`, 'label', 13.5),
-    constructItem(55, 119, `Upvotes`, 'label', 13.5),
-    constructItem(203, 119, `Bilibili Level`, 'label', 13.5),
-    constructItem(55, 154, `Signature`, 'label', 13.5),
+    constructItem(94, 44, `${name}`, 'title', 18),
+    constructItem(55, 84, `粉丝`, 'label', 13.5),
+    constructItem(203, 85, `播放量`, 'label', 13.5),
+    constructItem(55, 119, `点赞`, 'label', 13.5),
+    constructItem(203, 119, `等级`, 'label', 13.5),
+    constructItem(55, 154, `简介`, 'label', 13.5),
     constructItem(126, 84, `${fans}`, 'value', 15),
     constructItem(126, 119, `${likes}`, 'value', 15),
     constructItem(289, 86, `${views}`, 'value', 15),
